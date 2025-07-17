86e6b6f837be18d529fe8eb4ee7d908c6946dd7c
simran-dhiman01 <simrankaurdh5@gmail.com>
2025-07-18 00:27:56 +0530

    Removed vercel.json

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,11 @@
 import express from 'express'
 const app = express();
-import serverless from 'serverless-http'
 import dotenv from "dotenv"
 dotenv.config();
 import cookieparser from 'cookie-parser';
-import userRoutes from '../routes/UserRoutes.js'
+import userRoutes from './routes/UserRoutes.js'
 import cors from "cors"
-import { connectDB } from '../utils/db.js';
+import { connectDB } from './utils/db.js';
 
 
 //middlewares
@@ -27,5 +26,8 @@ app.get("/", (req, res) => {
 });
 
 
-
-export default serverless(app)
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    connectDB()
+    console.log(`Server running on PORT ${PORT}`);
+})
