292600238d7fb938c47afc4f708d8acfda4e24a3
simran-dhiman01 <simrankaurdh5@gmail.com>
2025-07-18 13:26:12 +0530

    Updated cors

--- backend/server.js
+++ backend/server.js
@@ -13,19 +13,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieparser())
 
-const allowedOrigins = [
-  process.env.FRONTEND_URL,
-  "http://localhost:4028/"
-]
+
 app.use(cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        return callback(new Error("Not allowed by CORS"));
-      }
-    }, // your frontend origin
+   origin: [
+      "http://localhost:4028",
+      process.env.FRONTEND_URL
+    ],
     credentials: true,               // allow cookies/credentials
 }))
 
