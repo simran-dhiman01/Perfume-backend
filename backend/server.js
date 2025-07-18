dfbd3429c28019d3a3aff2f9e55ce9d2573e78e8
simran-dhiman01 <simrankaurdh5@gmail.com>
2025-07-18 13:14:26 +0530

    Updated cors to run with local and deployed origin

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,20 @@ import { connectDB } from './utils/db.js';
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieparser())
+
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  "http://localhost:4028/"
+]
 app.use(cors({
-    origin: process.env.FRONTEND_URL, // your frontend origin
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    }, // your frontend origin
     credentials: true,               // allow cookies/credentials
 }))
 
